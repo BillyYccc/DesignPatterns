f56e8cf48df1d87512ec7e03031dea49a14bdec0
YuanChong <112487983@qq.com>
2017-04-22 15:25:22 +0800

    提交依赖倒置原则反例代码

---
+++ DIPDemo/DependenceInversionPrincipleDemoBadExample/src/main/java/Driver.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Billy Yuan on 2017/4/20.
+ * Email: billy112487983@gmail.com
+ */
+public class Driver {
+    //小明驾驶的方法
+    public void drive(Volkswagen volkswagen) {
+        volkswagen.run();
+    }
+}
---
+++ DIPDemo/DependenceInversionPrincipleDemoBadExample/src/main/java/Volkswagen.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Billy Yuan on 2017/4/20.
+ * Email: billy112487983@gmail.com
+ */
+public class Volkswagen {
+    //汽车运行的方法
+    public void run() {
+        System.out.println("大众汽车运行...");
+    }
+}
