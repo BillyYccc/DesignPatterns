6a551ebc872964cc0c1a4b4d9c8dcd62ddd8ee36
YuanChong <112487983@qq.com>
2017-04-22 16:33:24 +0800

    提交迪米特原则示例代码

---
+++ LoDDemo/GoodExample/Message.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Billy Yuan on 2017/4/22.
+ * Email: billy112487983@gmail.com
+ */
+public class Message {
+    public void sayHello(User user) {
+        user.sendMessage("Hello...");
+    }
+}
